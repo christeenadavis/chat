3c8b86e3ab49ffd7a1137983114ccbbdd5239ecd
christeenadavis <102350435+christeenadavis@users.noreply.github.com>
2023-01-07 12:39:38 +0530

    main

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => res.send('hello!'));
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+    socket.on('message', (msg) => {
+        console.log(msg);
+        socket.broadcast.emit('message-broadcast', msg);
+    });
 });
 
 http.listen(3000, () => {
